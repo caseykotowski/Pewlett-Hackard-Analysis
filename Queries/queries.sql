d5c733a750240a4cd0c2a640a7207c9fa3021925
caseykotowski <79807939+caseykotowski@users.noreply.github.com>
2021-05-06 22:26:14 -0500

    Completed HR data analysis

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -141,3 +141,34 @@ INNER JOIN departments as d
 ON (de.dept_no = d.dept_no)
 WHERE de.dept_no = 'd007';
 
+
+-- Challenge Code
+-- Find employee info for eligable retirements
+SELECT e.emp_no, 
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+--INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Number of retirements by title
+SELECT COUNT(title) as "Number of Retirements", title
+-- INTO retireing_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "Number of Retirements" DESC;
